21b4e5b311568adf16c39c3457fcabe566ba646c
Sasha Karataev <prumixxx@gmail.com>
2021-03-03 13:18:11 +0200

    create project

---
+++ src/main/java/ru/prumi/calcmanager/CalcmanagerApplication.java
@@ -0,0 +1,13 @@
+package ru.prumi.calcmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalcmanagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/prumi/calcmanager/CalcmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.prumi.calcmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
